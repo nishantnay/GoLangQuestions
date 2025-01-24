bdad493b7f22e89213386c57f6f16d44f6c73140
nishant.nayanam <nishant.nayanam@connectwise.com>
2025-01-24 15:31:31 +0530

    Added a better way to solve same problem

--- getSliceIndexes/main.go
+++ getSliceIndexes/main.go
@@ -2,45 +2,61 @@ package main
 
 import "fmt"
 
-func twoSum(nums []int, target int) []int {
-	if len(nums) <2{
-		return []int {}
+func basicCalculation(nums []int, target int) []int {
+	if len(nums) < 2 {
+		return []int{}
 	}
 
-	for i:=0; i< len(nums); i++{
-		for j:=i+1; j<len(nums)-1; j++{
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums)-1; j++ {
 
-			if nums[i]+nums[j] == target{
+			if nums[i]+nums[j] == target {
 				return []int{i, j}
 			}
 		}
 	}
-	return []int {}
-    
+	return []int{}
+
+}
+func twoSum(nums []int, target int) [][]int {
+
+	traversed := make(map[int]int)
+	returnSlices := make([][]int, 0)
+	for index, value := range nums {
+		traversed[value] = index
+		if val, ok := traversed[target-value]; ok {
+			returnSlices = append(returnSlices, []int{val, index})
+		}
+
+	}
+
+	return returnSlices
 }
 
-func main(){
+func main() {
 
-	// Let there be a slice of integeres as 
+	// Let there be a slice of integeres as
 
-	s:=[]int{2,7,11,15}
-	// and 
-	target:=9
+	s := []int{20, 11, 7, 6, 15, 3, 2}
+	// and
+	target := 15
 
-	// Send the parameters to twoSun function to get the two indexes of the Slice 
-	returnedSlice:= twoSum(s, target)
+	// Send the parameters to twoSun function to get the two indexes of the Slice
+	returnedSlice := twoSum(s, target)
+	fmt.Println(returnedSlice)
+	for _, v := range returnedSlice {
+		var sumOfIndexValues = 0
+		for _, value := range v {
 
+			sumOfIndexValues += s[value]
 
-	var sumOfIndexValues=0
-	for _,v:=range returnedSlice{
-		sumOfIndexValues +=s[v]
+		}
+		if sumOfIndexValues == target {
+			
+			fmt.Println("Our logic is working fine for Indexes: ",v)
+		} else {
+			fmt.Println("Evaluate your logic for Indexes: ",v)
+		}
 	}
-	if sumOfIndexValues==target{
-		fmt.Println(returnedSlice)
-		fmt.Println("Our logic is working fine!")
-	}else{
-		fmt.Println(returnedSlice)
-		fmt.Println("Evaluate your logic!")
-	}
+
 }
-
